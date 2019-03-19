f76720f156a166aeb2abf573f672a9b3d3c08b11
Nikhil Prasad <nikhilpd791@gmail.com>
2019-03-19 17:05:43 +0530

    Add files via upload

--- ELearning.sql
+++ ELearning.sql
@@ -2,10 +2,14 @@
 sqlplus / as sysdba
 username: system
 password: pune
-grant connect,resource,dba to ELearning identified by management;
+grant connect.resource,dba to ELearning identified by management;
 
 --Create Table Address
 create table EAddress(Address_Id Number(3) not null, Address_Line_1 varchar(50) not null, Address_Line_2 varchar(50), City varchar(15) not null, State varchar(15), Country varchar(15) not null, Pin_Code Number(6) not null, primary key(Address_Id));
+
+--Auto-increament for Table Address
+create sequence Address_seq start with 1 increment by 1;
+-- use "Address_seq.nextval" to get next value of Address id
 
 --Create Table User
 create table EUser(User_Id Number(3) not null,Name varchar(50) not null,Age Number(2) not null, Gender varchar(7), ContactNumber Number(10), Address Number(5) not null, Password varchar(15) not null, Role Number(1) not null, primary key(User_Id));
@@ -14,7 +18,7 @@ Alter table EUser add foreign key(Address) references EAddress(Address_Id);
 
 --Auto-Increment for Table User 
 create sequence EUser_seq start with 1 increment by 1;
-
+--use "EUser_seq.nextval" to get next value of user id
 --Create Table Role
 create table Role(Name varchar(50) not null, Code varchar(3) not null);
 
